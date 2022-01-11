243744a3955c6e0dc1937efd53a1093a86a93643
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-11 22:42:07 +0300

    test

--- workflow/scripts/SAM_trimmer.py
+++ workflow/scripts/SAM_trimmer.py
@@ -35,10 +35,10 @@ def main():
         r_qname, r_flag, r_rname, r_pos, r_mapq, r_cigar, r_rnext, r_pnext, r_tlen, r_seq, r_qual = reverse[:11]
         r_bitwise_flags = '\t'.join(reverse[11:])
 
-        print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq))
-        print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq))
-        print("----")
-        if f_pos == args.pos and f_seq.startswith(pattern):
+        if f_pos == '2182' and f_seq.startswith(pattern):
+            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq), f_mapq)
+            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq), r_mapq)
+            print("----")
             if int(f_tlen) > 0:
                 f_tlen = str(int(f_tlen) - pattern_len)
                 f_pos = str(int(f_pos) + pattern_len)
@@ -53,7 +53,7 @@ def main():
             if int(r_tlen) > 0:
                 r_tlen = str(int(r_tlen) - pattern_len)
                 r_pos = str(int(r_pos) + pattern_len)
-            elif int(f_tlen) < 0:
+            elif int(r_tlen) < 0:
                 r_tlen = str(int(r_tlen) + pattern_len)
                 r_pos = str(int(r_pos) - pattern_len)
             else:
@@ -64,14 +64,17 @@ def main():
             r_pnext = f_pos
             f_pnext = r_pos
             print("F:")
-            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq))
-            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq))
+            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq), f_mapq)
+            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq), r_mapq)
             print("--------------")
-        elif r_pos == args.pos and r_seq.startswith(pattern):
+        elif r_pos == '2182' and r_seq.startswith(pattern):
+            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq), f_mapq)
+            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq), r_mapq)
+            print("----")
             if int(r_tlen) > 0:
                 r_tlen = str(int(r_tlen) - pattern_len)
                 r_pos = str(int(r_pos) + pattern_len)
-            elif int(f_tlen) < 0:
+            elif int(r_tlen) < 0:
                 r_tlen = str(int(r_tlen) + pattern_len)
                 r_pos = str(int(r_pos) - pattern_len)
             else:
@@ -93,8 +96,8 @@ def main():
             r_pnext = f_pos
             f_pnext = r_pos
             print("R:")
-            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq))
-            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq))
+            print("F", f_tlen, "|", f_pos, f_pnext, f_cigar, len(f_seq), f_mapq)
+            print("R", r_tlen, "|", r_pos, r_pnext, r_cigar, len(r_seq), r_mapq)
             print("--------------")
         forward = "\t".join([f_qname, f_flag, f_rname, f_pos, f_mapq, f_cigar, f_rnext, f_pnext, f_tlen, f_seq, f_qual, f_bitwise_flags])
         reverse = "\t".join([r_qname, r_flag, r_rname, r_pos, r_mapq, r_cigar, r_rnext, r_pnext, r_tlen, r_seq, r_qual, r_bitwise_flags])
