913d378e58500133c8771f77ff62dd0b4dd6c34f
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-11 14:12:55 +0300

    test

--- workflow/scripts/SAM_trimmer.py
+++ workflow/scripts/SAM_trimmer.py
@@ -34,20 +34,23 @@ def main():
         f_bitwise_flags = '\t'.join(forward[11:])
         r_qname, r_flag, r_rname, r_pos, r_mapq, r_cigar, r_rnext, r_pnext, r_tlen, r_seq, r_qual = reverse[:11]
         r_bitwise_flags = '\t'.join(reverse[11:])
-        if (f_pos == args.pos or r_pos == args.pos) and (f_seq.startswith(args.pattern) or r_seq.startswith(args.pattern)):
-            if int(f_tlen) > 0: # f_* is a forward
-                f_tlen = str(int(f_tlen) - pattern_len)
-                f_pos = str(int(f_pos) + pattern_len)
-                r_tlen = str(int(r_tlen) + pattern_len)
-                r_pos = str(int(r_pos) - pattern_len)
-            elif int(f_tlen) < 0: # f_* is a reverse
-                f_tlen = str(int(f_tlen) + pattern_len)
-                f_pos = str(int(f_pos) - pattern_len)
-                r_tlen = str(int(r_tlen) - pattern_len)
-                r_pos = str(int(r_pos) + pattern_len)
-            f_seq, r_seq = f_seq[pattern_len:], r_seq[pattern_len:]
-            f_qual, r_qual = f_qual[pattern_len:], r_qual[pattern_len:]
-            f_cigar, r_cigar = cigar_left_trimmer(f_cigar, pattern_len), cigar_left_trimmer(r_cigar, pattern_len)
+        if (f_pos == args.pos or r_pos == args.pos) and (f_seq.startswith(args.pattern) or r_seq.startswith(args.pattern)) and int(f_tlen) > 0:
+            # if int(f_tlen) > 0: # f_* is a forward
+            f_tlen = str(int(f_tlen) - pattern_len)
+            f_pos = str(int(f_pos) + pattern_len)
+            #     r_tlen = str(int(r_tlen) + pattern_len)
+            #     r_pos = str(int(r_pos) - pattern_len)
+            # elif int(f_tlen) < 0: # f_* is a reverse
+            #     f_tlen = str(int(f_tlen) + pattern_len)
+            #     f_pos = str(int(f_pos) - pattern_len)
+            #     r_tlen = str(int(r_tlen) - pattern_len)
+            #     r_pos = str(int(r_pos) + pattern_len)
+            f_seq = f_seq[pattern_len:]
+            f_qual = f_qual[pattern_len:]
+            f_cigar = cigar_left_trimmer(f_cigar, pattern_len)
+            # f_seq, r_seq = f_seq[pattern_len:], r_seq[pattern_len:]
+            # f_qual, r_qual = f_qual[pattern_len:], r_qual[pattern_len:]
+            # f_cigar, r_cigar = cigar_left_trimmer(f_cigar, pattern_len), cigar_left_trimmer(r_cigar, pattern_len)
         forward = "\t".join([f_qname, f_flag, f_rname, f_pos, f_mapq, f_cigar, f_rnext, f_pnext, f_tlen, f_seq, f_qual, f_bitwise_flags])
         reverse = "\t".join([r_qname, r_flag, r_rname, r_pos, r_mapq, r_cigar, r_rnext, r_pnext, r_tlen, r_seq, r_qual, r_bitwise_flags])
         outfile.write("%s\n" % forward)
