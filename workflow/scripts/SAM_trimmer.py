35a260b763c4d42ca676d7333452c211bedccab2
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-11 11:54:52 +0300

    test

--- workflow/scripts/SAM_trimmer.py
+++ workflow/scripts/SAM_trimmer.py
@@ -36,14 +36,14 @@ def main():
         r_bitwise_flags = '\t'.join(reverse[11:])
         if (f_pos == args.pos or r_pos == args.pos) and (f_seq.startswith(args.pattern) or r_seq.startswith(args.pattern)):
             if int(f_tlen) > 0:
-                f_tlen = str(int(f_tlen) - pattern_len)
+                # f_tlen = str(int(f_tlen) - pattern_len)
                 f_pos = str(int(f_pos) + pattern_len)
-                r_tlen = str(int(r_tlen) + pattern_len)
+                # r_tlen = str(int(r_tlen) + pattern_len)
                 r_pos = str(int(r_pos) - pattern_len)
             elif int(f_tlen) < 0:
-                f_tlen = str(int(f_tlen) + pattern_len)
+                # f_tlen = str(int(f_tlen) + pattern_len)
                 f_pos = str(int(f_pos) - pattern_len)
-                r_tlen = str(int(r_tlen) - pattern_len)
+                # r_tlen = str(int(r_tlen) - pattern_len)
                 r_pos = str(int(r_pos) + pattern_len)
             f_seq, r_seq = f_seq[pattern_len:], r_seq[pattern_len:]
             f_qual, r_qual = f_qual[pattern_len:], r_qual[pattern_len:]
