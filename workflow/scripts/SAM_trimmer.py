fb18bcd1f6868791b3f7b6123041a8c5515b54a3
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-12 14:21:55 +0300

    test

--- workflow/scripts/SAM_trimmer.py
+++ workflow/scripts/SAM_trimmer.py
@@ -35,11 +35,9 @@ def main():
         r_bitwise_flags = '\t'.join(reverse[11:])
 
         if f_pos == args.pos and f_seq.startswith(args.pattern) and int(f_tlen) > 0:
-            f_seq = f_seq[pattern_len:]
-            f_qual = f_qual[pattern_len:]
+            f_seq, r_seq = f_seq[pattern_len:], r_seq[pattern_len:]
+            f_qual, r_qual = f_qual[pattern_len:], r_qual[pattern_len:]
             f_cigar = cigar_left_trimmer(f_cigar, pattern_len)
-            r_seq = r_seq[pattern_len:]
-            r_qual = r_qual[pattern_len:]
             r_cigar = cigar_left_trimmer(r_cigar, pattern_len)
             f_tlen = str(int(f_tlen) - pattern_len)
             r_tlen = str(int(r_tlen) + pattern_len)
@@ -48,16 +46,14 @@ def main():
             r_pnext = f_pos
             f_pnext = r_pos
         elif r_pos == args.pos and r_seq.startswith(args.pattern) and int(r_tlen) > 0:
-            f_seq = f_seq[pattern_len:]
-            f_qual = f_qual[pattern_len:]
+            f_seq, r_seq = f_seq[pattern_len:], r_seq[pattern_len:]
+            f_qual, r_qual = f_qual[pattern_len:], r_qual[pattern_len:]
             f_cigar = cigar_left_trimmer(f_cigar, pattern_len)
-            r_seq = r_seq[pattern_len:]
-            r_qual = r_qual[pattern_len:]
             r_cigar = cigar_left_trimmer(r_cigar, pattern_len)
-            f_tlen = str(int(f_tlen) - pattern_len)
-            r_tlen = str(int(r_tlen) + pattern_len)
-            f_pos = str(int(f_pos) - pattern_len)
-            r_pos = str(int(r_pos) + pattern_len)
+            f_tlen = str(int(f_tlen) + pattern_len)
+            r_tlen = str(int(r_tlen) - pattern_len)
+            f_pos = str(int(f_pos) + pattern_len)
+            r_pos = str(int(r_pos) - pattern_len)
             r_pnext = f_pos
             f_pnext = r_pos
 
